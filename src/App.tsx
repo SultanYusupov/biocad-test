1799fa8f790687611ba35080d25fb8c477ca4ca0
Sultan <s.yusupov.prog@solo-it.ru>
2025-06-11 16:48:34 +0400

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Form} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const [firstInput, setFirstInput] = useState<string>();
+    const [secondInput, setSecondInput] = useState<string>();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Form>
+            <Form.Group className="mb-3" controlId="aminoAcids1">
+                <Form.Label column={'lg'}>Input1</Form.Label>
+                <Form.Control type="text" autoComplete={'true'} value={firstInput} onChange={(e) => setFirstInput(e.target.value)} required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="aminoAcids2">
+                <Form.Label column={'lg'}>Input1</Form.Label>
+                <Form.Control type="text" autoComplete={'true'} value={secondInput} onChange={(e) => setSecondInput(e.target.value)} required />
+            </Form.Group>
+        </Form>
     </>
   )
 }

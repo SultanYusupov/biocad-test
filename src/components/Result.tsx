e45a8fc07f0fbd35e89e914a4a5d2bb295a7ae6a
Sultan <s.yusupov.prog@solo-it.ru>
2025-06-13 23:34:35 +0400

    character transfer

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -7,17 +7,13 @@ export default function Result({data}:FieldValues) {
                 return 'initial';
         }
         return(
-            <div style={{marginTop: '20px'}}>
-                <p>
-                    {input1.split('').map((ch: keyof AminoAcidColorScheme, index: number) => {
-                        return <span key={index} style={{backgroundColor: colorScheme[ch], borderRadius: '2px'}}>{ch}</span>
-                    })}
-                </p>
-                <p>
-                    {input2.split('').map((ch: keyof AminoAcidColorScheme, index: number) => {
-                        return <span key={index} style={{backgroundColor: findDifferencies(index), borderRadius: '2px'}}>{ch}</span>
-                    })}
-                </p>
+            <div style={{marginTop: '20px', display: 'flex', flexWrap: 'wrap'}}>
+                    {input1.split('').map((ch: keyof AminoAcidColorScheme, index: number) => (
+                        <div key={index} style={{display: 'flex', flexDirection: 'column', marginRight: '2px'}}>
+                                <span style={{backgroundColor: colorScheme[ch], borderRadius: '2px', marginBottom: '2px'}}>{input1[index]}</span>
+                                <span style={{backgroundColor: findDifferencies(index), borderRadius: '2px', marginBottom: '2px'}}>{input2[index]}</span>
+                        </div>
+                    ))}
             </div>
         )
 }

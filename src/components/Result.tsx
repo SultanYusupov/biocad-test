d32955ba196b1b7316baed7ffa66db6e3f04735f
Sultan <s.yusupov.prog@solo-it.ru>
2025-06-13 19:04:51 +0400

    highlighted the different characters

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -2,6 +2,10 @@ import type {FieldValues} from "react-hook-form";
 import {AMINO_ACID_COLOR_SCHEME as colorScheme, type AminoAcidColorScheme} from "../constants/validation"
 export default function Result({data}:FieldValues) {
         const {input1, input2} = data;
+        function findDifferencies(position: number) {
+                if (input2[position] !== input1[position]) return '#f80000';
+                return 'initial';
+        }
         return(
             <div>
                 <p>
@@ -11,7 +15,7 @@ export default function Result({data}:FieldValues) {
                 </p>
                 <p>
                     {input2.split('').map((ch: keyof AminoAcidColorScheme, index: number) => {
-                        return <span key={index} style={{backgroundColor: 'initial'}}>{ch}</span>
+                        return <span key={index} style={{backgroundColor: findDifferencies(index), borderRadius: '2px'}}>{ch}</span>
                     })}
                 </p>
             </div>
--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -1,11 +1,3 @@
-/*const AMINO_ACID_COLOR_SCHEME = {
-    '#FFEA00': ['C'],
-    '#67E4A6': ['A', 'I', 'L', 'M', 'F', 'W', 'Y', 'V', 'P'],
-    '#C4C4C4': ['G'],
-    '#FCCAC': ['D', 'E'],
-    '#BB99FF': ['K', 'R'],
-    '#80BFFF': ['S', 'T', 'H', 'Q', 'N']
-};*/
 export type AminoAcidColorScheme = {
     [key in 'A' | 'R' | 'N' | 'D' | 'C' | 'E' | 'Q' | 'G' | 'H' | 'I' | 'L' | 'K' | 'M' | 'F' | 'P' | 'S' | 'T' | 'W' | 'Y' | 'V']: string;
 };
@@ -14,7 +6,7 @@ export const AMINO_ACID_COLOR_SCHEME:AminoAcidColorScheme = {
     , 'Q': '#80BFFF', 'G': '#C4C4C4', 'H': '#80BFFF', 'I': '#67E4A6', 'L': '#67E4A6', 'K': '#BB99FF',
     'M': '#67E4A6', 'F': '#67E4A6', 'P': '#67E4A6', 'S': '#80BFFF', 'T': '#80BFFF', 'W': '#67E4A6',
     'Y': '#67E4A6', 'V': '#67E4A6'
-}
+};
 // const ALLOWED_CHARACTERS = ['A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', '-'];
 const ALLOWED_CHARACTERS = Object.keys(AMINO_ACID_COLOR_SCHEME).concat(['-']);
 export const ALLOWED_CHARACTERS_REGEX = new RegExp(`[${ALLOWED_CHARACTERS.join('')}]`, 'i');

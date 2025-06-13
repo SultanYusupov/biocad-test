0276145476ee3d09189c54679c96e66d133500e2
Sultan <s.yusupov.prog@solo-it.ru>
2025-06-13 16:02:37 +0400

    validation, error messages

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,17 @@ import Result from "./components/Result.tsx";
 import {useState} from "react";
 
 function App() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, setError, formState: { errors } } = useForm();
     const { input1, input2 } = useValidation();
     const [formData, setFormData] = useState<FieldValues>();
     const onSubmit = ((data:FieldValues) => {
+        if (data.input1.length !== data.input2.length) {
+            setError("input2", {
+                type: "manual",
+                message: "Длина последовательностей в обоих полях должна быть одинаковой"
+            });
+            return;
+        }
         setFormData(data);
     });
   return (
@@ -18,11 +25,17 @@ function App() {
         <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3" controlId="aminoAcids1">
                 <Form.Label column={'lg'}>Input1</Form.Label>
-                <Form.Control type="text" autoComplete={'true'} {...register("input1", input1)} required />
+                <Form.Control type="text" autoComplete={'true'} {...register("input1", input1)} required isInvalid={!!errors?.input1} />
+                <Form.Control.Feedback type="invalid">
+                    {errors?.input1?.message?.toString()}
+                </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="aminoAcids2">
                 <Form.Label column={'lg'}>Input2</Form.Label>
-                <Form.Control type="text" autoComplete={'true'} {...register("input2", input2)} required />
+                <Form.Control type="text" autoComplete={'true'} {...register("input2", input2)} required isInvalid={!!errors?.input2} />
+                <Form.Control.Feedback type="invalid">
+                    {errors?.input2?.message?.toString()}
+                </Form.Control.Feedback>
             </Form.Group>
             <Button onClick={handleSubmit(onSubmit)}>Enter</Button>
         </Form>
